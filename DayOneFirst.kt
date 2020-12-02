86bf2a4a4a074ead278616a9738c1f1194f07482
dbilgin <denizcanbilgin@gmail.com>
2020-12-02 01:23:54 +0100

    day one complete

---
+++ DayOneFirst.kt
@@ -0,0 +1,26 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    val inputStream: InputStream = File("./inputs/DayOne").inputStream()
+    val lineList = mutableListOf<Int>()
+    inputStream.bufferedReader().forEachLine { lineList.add(it.toInt()) }
+
+    lineList.forEach {
+        val result = scanList(it, lineList)
+        if (result != null) {
+            println(result * it)
+            return
+        }
+    }
+}
+
+fun scanList(currentItem: Int, list: List<Int>): Int? {
+    list.forEach {
+        if (currentItem + it == 2020) {
+            return it
+        }
+    }
+
+    return null
+}
---
+++ DayOneSecond.kt
@@ -0,0 +1,33 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+    val inputStream: InputStream = File("./inputs/DayOne").inputStream()
+    val lineList = mutableListOf<Int>()
+    inputStream.bufferedReader().forEachLine { lineList.add(it.toInt()) }
+
+    lineList.forEach {
+        val result = recursiveScan(it, lineList, Condition.SMALLER)
+        if(result != null) {
+            println(result * it)
+            return
+        }
+    }
+}
+
+fun recursiveScan(currentItem: Int, list: List<Int>, condition: Condition): Int? {
+    list.forEach {
+        if (condition == Condition.EQUALS && currentItem + it == 2020) {
+            return it
+        } else if (condition == Condition.SMALLER && currentItem + it < 2020) {
+            val recursiveResult = recursiveScan(currentItem + it, list, Condition.EQUALS)
+            if(recursiveResult != null) return it * recursiveResult
+        }
+    }
+
+    return null
+}
+
+enum class Condition {
+  SMALLER, EQUALS
+}
