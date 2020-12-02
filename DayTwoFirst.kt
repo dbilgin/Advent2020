9df6e8ee11c9537ba112d62d68b173a038bc979a
dbilgin <denizcanbilgin@gmail.com>
2020-12-03 00:35:09 +0100

    second day of advent

---
+++ DayTwoFirst.kt
@@ -0,0 +1,20 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+  val inputStream: InputStream = File("./inputs/DayTwo").inputStream()
+  val lineList = mutableListOf<String>()
+  inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
+  var count = 0
+  lineList.forEach {
+      val splitResult = it.split("-|\\: | ".toRegex())
+      val countResult = splitResult[3].filter { it.toString() == splitResult[2] }.count()
+
+      if (countResult >= splitResult[0].toInt() && countResult <= splitResult[1].toInt()) {
+        count++
+      }
+  }
+
+    println(count)
+}
---
+++ DayTwoSecond.kt
@@ -0,0 +1,25 @@
+import java.io.File
+import java.io.InputStream
+
+fun main() {
+  val inputStream: InputStream = File("./inputs/DayTwo").inputStream()
+  val lineList = mutableListOf<String>()
+  inputStream.bufferedReader().forEachLine { lineList.add(it) }
+
+  var count = 0
+  lineList.forEach {
+      val splitResult = it.split("-|\\: | ".toRegex())
+
+      val firstIndex = splitResult[0].toInt() - 1
+      val secondIndex = splitResult[1].toInt() - 1
+
+      if (
+        (splitResult[2].contains(splitResult[3][firstIndex]) && !splitResult[2].contains(splitResult[3][secondIndex])) ||
+        (!splitResult[2].contains(splitResult[3][firstIndex]) && splitResult[2].contains(splitResult[3][secondIndex]))
+      ) {
+        count++
+      }
+  }
+
+    println(count)
+}
